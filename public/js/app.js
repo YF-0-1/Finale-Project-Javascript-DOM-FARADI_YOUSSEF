041d9d6d462cd2a129c8d52c9090ae6f88c71bfb
Youssef-faradi <faradiyoussef1@gmail.com>
2024-01-19 16:45:47 +0100

    section4

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,6 @@
 //& Header
 //* sideBar
+const body = document.body
 const navbarBtn = document.querySelector('.navbar-toggler')
 const navElements = document.querySelector('.navigation')
 
@@ -35,8 +36,15 @@ connexion.addEventListener("click", ()=> {
     if(navElements.classList.contains('nav-elements')){
         navElements.classList.remove('nav-elements');
         navElements.classList.add('nav-hide');
+        
     }
     modale.classList.toggle('d-none');
+    if (!modale.classList.contains('d-none')) {
+        body.style.overflow = 'hidden';
+    } else {
+        body.style.overflow = '';
+    }
+
     signin.addEventListener("click", () => {
         event.stopPropagation();
         if(signinDiv.classList.contains('d-none')){
@@ -69,10 +77,50 @@ connexion.addEventListener("click", ()=> {
 
     clear.addEventListener("click", () =>{
         modale.classList.add('d-none');
+        if (!modale.classList.contains('d-none')) {
+            body.style.overflow = 'hidden';
+        } else {
+            body.style.overflow = '';
+        }
     })
 })
 
 
 
 // & main
-// *section 1
+// * caroussel-1
+
+const BTNS = document.querySelectorAll(".btns_caroussel_1 button")
+
+const caroussel = document.querySelector(".caroussel_1_middle")
+const firstimg = caroussel.querySelector(".img-div").offsetWidth;
+const carouselChildrens = Array.from(document.querySelectorAll(".caroussel_1_middle .img-div"));
+const galleryCardWidth = document.querySelector('.img-div div').offsetWidth;
+
+
+// carouselChildrens.slice(0, 1).forEach(card => {
+//     const clonedCard = card.cloneNode(true);
+//     caroussel.appendChild(clonedCard);
+// });
+
+BTNS.forEach(btn => {
+    btn.addEventListener('click',()=>{
+        if (btn.id === "btn-1") {
+            let elemt = caroussel.lastElementChild
+            const clonedCard = elemt.cloneNode(true);
+            caroussel.insertAdjacentElement("afterbegin", clonedCard);
+            elemt.style.transform = `translateX(${galleryCardWidth}px)`;
+
+            // caroussel.scrollLeft -= 452
+            // caroussel.removeChild(caroussel.lastElementChild);
+        }else if((btn.id === "btn-3")){
+            caroussel.scrollLeft -= 452
+        }else{
+            console.log("messi GOAT")
+        }
+    })
+});
+
+
+
+
